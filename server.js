d1c3b4cbc9d86ea01ab43de4862037c178fbb92c
Mary Sabio <mlssabio21@gmail.com>
2017-11-12 04:41:26 +0800

    Added functions for remaining APIs.

--- server.js
+++ server.js
@@ -75,10 +75,35 @@ app.post("/api/quotes", function(req, res) {
  */
 
 app.get("/api/quotes/:id", function(req, res) {
+  db.collection(QUOTES_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to get quote");
+    } else {
+      res.status(200).json(doc);
+    }
+  });
 });
 
 app.put("/api/quotes/:id", function(req, res) {
+  var updateDoc = req.body;
+  delete updateDoc._id;
+
+  db.collection(QUOTES_COLLECTION).updateOne({_id: new ObjectID(req.params.id)}, updateDoc, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to update quote");
+    } else {
+      updateDoc._id = req.params.id;
+      res.status(200).json(updateDoc);
+    }
+  });
 });
 
 app.delete("/api/quotes/:id", function(req, res) {
+  db.collection(QUOTES_COLLECTION).deleteOne({_id: new ObjectID(req.params.id)}, function(err, result) {
+    if (err) {
+      handleError(res, err.message, "Failed to delete quote");
+    } else {
+      res.status(200).json(req.params.id);
+    }
+  });
 });
